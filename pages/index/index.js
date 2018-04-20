73be5ef099ba80555c30047f25bb075cc50d58f8
李正先 <hzlizx@tairanchina.com>
2018-04-20 11:42:20 +0800

    启动时检查新版本

--- pages/index/index.js
+++ pages/index/index.js
@@ -158,6 +158,7 @@ Page(Object.assign({}, Zan.TopTips, {
         }
       })
     }
+    this.checkUpdate();
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -293,5 +294,36 @@ Page(Object.assign({}, Zan.TopTips, {
         // 转发失败
       }
     }
+  },
+  checkUpdate(){
+    console.log("开始检查更新...")
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新的版本下载失败
+      wx.showToast({
+        title: '新的版本下载失败',
+        icon: 'none',
+        duration: 2000
+      })
+    })
   }
 }))
