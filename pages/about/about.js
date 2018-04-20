ca8091a23b1faf2047eb7d534788a5dec631625f
李正先 <hzlizx@tairanchina.com>
2018-04-20 11:07:21 +0800

    修改部分页面排版

--- pages/about/about.js
+++ pages/about/about.js
@@ -29,5 +29,37 @@ Page({
         })
       }
     })
+  },
+  copy: function (e) {
+    var data = ""
+    var title = ""
+    switch (e.currentTarget.dataset.index){
+      case "0":
+        data = "lizhengxian2005@gmail.com"
+        title = "已复制邮箱"
+        break;
+      case "1":
+        data = "https://github.com/lzx2005"
+        title = "已复制Github"
+        break;
+      case "2":
+        data = "https://weibo.com/2557929062"
+        title = "已复制微博地址"
+        break;
+      case "3":
+        data = "https://www.zhihu.com/people/lzx2005"
+        title = "已复制知乎地址"
+        break;
+    }
+    wx.setClipboardData({
+      data: data,
+      success: function (res) {
+        wx.showToast({
+          title: title,
+          icon: 'success',
+          duration: 2000
+        })
+      }
+    })
   }
 })
