6de537d5d5dca2f7e476cfc7489d5e2d1c7b9136
李正先 <hzlizx@tairanchina.com>
2018-04-20 11:50:34 +0800

    启动时检查新版本

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,38 @@ var Zan = require('../../wxss/dist/index');
 const config = require('./config');
 const app = getApp()
 const timer = null
+
+
+console.log("开始检查更新...")
+const updateManager = wx.getUpdateManager()
+
+updateManager.onCheckForUpdate(function (res) {
+  // 请求完新版本信息的回调
+  console.log(res.hasUpdate)
+})
+
+updateManager.onUpdateReady(function () {
+  wx.showModal({
+    title: '更新提示',
+    content: '新版本已经准备好，是否重启应用？',
+    success: function (res) {
+      if (res.confirm) {
+        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+        updateManager.applyUpdate()
+      }
+    }
+  })
+})
+
+updateManager.onUpdateFailed(function () {
+  // 新的版本下载失败
+  wx.showToast({
+    title: '新的版本下载失败',
+    icon: 'none',
+    duration: 2000
+  })
+})
+
 Page(Object.assign({}, Zan.TopTips, {
   data: {
     config,
@@ -296,34 +328,5 @@ Page(Object.assign({}, Zan.TopTips, {
     }
   },
   checkUpdate(){
-    console.log("开始检查更新...")
-    const updateManager = wx.getUpdateManager()
-
-    updateManager.onCheckForUpdate(function (res) {
-      // 请求完新版本信息的回调
-      console.log(res.hasUpdate)
-    })
-
-    updateManager.onUpdateReady(function () {
-      wx.showModal({
-        title: '更新提示',
-        content: '新版本已经准备好，是否重启应用？',
-        success: function (res) {
-          if (res.confirm) {
-            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-            updateManager.applyUpdate()
-          }
-        }
-      })
-    })
-
-    updateManager.onUpdateFailed(function () {
-      // 新的版本下载失败
-      wx.showToast({
-        title: '新的版本下载失败',
-        icon: 'none',
-        duration: 2000
-      })
-    })
   }
 }))
